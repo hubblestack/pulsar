636b09529aab8dd195969a28df5c83c73531112e
Colton Myers <colton.myers@gmail.com>
2016-09-06 09:40:18 -0600

    Pull salt.utils.dictupdate.update to prevent version issues

--- _beacons/pulsar.py
+++ _beacons/pulsar.py
@@ -177,10 +177,10 @@ def beacon(config):
                 cpath = __salt__['cp.cache_file'](path)
                 if os.path.isfile(cpath):
                     with open(cpath, 'r') as f:
-                        new_config = salt.utils.dictupdate.update(new_config,
-                                                                  yaml.safe_load(f),
-                                                                  recursive_update=True,
-                                                                  merge_lists=True)
+                        new_config = _dict_update(new_config,
+                                                  yaml.safe_load(f),
+                                                  recursive_update=True,
+                                                  merge_lists=True)
                 else:
                     log.error('Path {0} does not exist or is not a file'.format(cpath))
         else:
@@ -346,3 +346,53 @@ def beacon(config):
     else:
         # Return event data
         return ret
+
+
+def _dict_update(dest, upd, recursive_update=True, merge_lists=False):
+    '''
+    Recursive version of the default dict.update
+
+    Merges upd recursively into dest
+
+    If recursive_update=False, will use the classic dict.update, or fall back
+    on a manual merge (helpful for non-dict types like FunctionWrapper)
+
+    If merge_lists=True, will aggregate list object types instead of replace.
+    This behavior is only activated when recursive_update=True. By default
+    merge_lists=False.
+    '''
+    if (not isinstance(dest, collections.Mapping)) \
+            or (not isinstance(upd, collections.Mapping)):
+        raise TypeError('Cannot update using non-dict types in dictupdate.update()')
+    updkeys = list(upd.keys())
+    if not set(list(dest.keys())) & set(updkeys):
+        recursive_update = False
+    if recursive_update:
+        for key in updkeys:
+            val = upd[key]
+            try:
+                dest_subkey = dest.get(key, None)
+            except AttributeError:
+                dest_subkey = None
+            if isinstance(dest_subkey, collections.Mapping) \
+                    and isinstance(val, collections.Mapping):
+                ret = update(dest_subkey, val, merge_lists=merge_lists)
+                dest[key] = ret
+            elif isinstance(dest_subkey, list) \
+                     and isinstance(val, list):
+                if merge_lists:
+                    dest[key] = dest.get(key, []) + val
+                else:
+                    dest[key] = upd[key]
+            else:
+                dest[key] = upd[key]
+        return dest
+    else:
+        try:
+            for k in upd.keys():
+                dest[k] = upd[k]
+        except AttributeError:
+            # this mapping is not a dict
+            for k in upd:
+                dest[k] = upd[k]
+        return dest
